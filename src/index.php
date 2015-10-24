514739f33c6e2918daa92e2fe760d6b66127d736
youngershen <younger.x.shen@gmail.com>
2015-10-24 15:50:39 +0800

    init

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+/* PROJECT  : laravel-wechat-sdk
+ * FILENAME : index.php
+ * DATE     : 2015/10/24 15:45
+ * AUTHOR   : younger shen
+ * EMAIL    : younger@younger.x.shen@gmail.com
+ * Copyright: Shenzhen Universal Youth technology LTD  
+ */
+
+namespace laravel_wechat_sdk\test;
